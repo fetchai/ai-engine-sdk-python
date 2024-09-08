abb2a6307e9f665361d94d4ddb3715f6a8e90262
xavi <xpeiro92@gmail.com>
2024-09-08 12:31:35 +0200

    merge with master + tests + fixes and scripts

--- ai_engine_sdk/client.py
+++ ai_engine_sdk/client.py
@@ -273,10 +273,11 @@ class Session:
 
     async def execute_function(self, function_ids: list[str], objective: str, context: str|None = None):
         await self._submit_message(
-            payload=ApiUserMessageExecuteFunctions.parse_obj({
+            payload=ApiUserMessageExecuteFunctions.model_validate({
                 "functions": function_ids,
                 "objective": objective,
-                "context": context or ""
+                "context": context or "",
+                'session_id': self.session_id,
             })
         )
 
@@ -284,6 +285,7 @@ class AiEngine:
     def __init__(self, api_key: str, options: Optional[dict] = None):
         self._api_base_url = options.get('api_base_url') if options and 'api_base_url' in options else default_api_base_url
         self._api_key = api_key
+
 
     ####
     # Function groups
@@ -391,7 +393,7 @@ class AiEngine:
         if "functions" in raw_response:
             list(
                 map(
-                    lambda function_name: FunctionGroupFunctions.parse_obj({"name": function_name}),
+                    lambda function_name: FunctionGroupFunctions.model_validate({"name": function_name}),
                     raw_response["functions"]
                 )
             )
